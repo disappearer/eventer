4d97fb64c5002590de062ed27da55a899e23720c
disappearer <aleksa47@gmail.com>
2020-04-06 17:03:03 +0200

    Add loaders

--- apps/eventer_web/assets/js/src/App.tsx
+++ apps/eventer_web/assets/js/src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from './common/store';
-import Loader from './Loader';
 import { ThemeProvider } from 'styled-components';
+import store from './common/store';
+import RouterWithAuthCheck from './RouterWithAuthCheck';
 import theme from './theme';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <ThemeProvider theme={theme}>
-
-      <Loader />
+        <RouterWithAuthCheck />
       </ThemeProvider>
     </Provider>
   );
