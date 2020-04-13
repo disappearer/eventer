912ba979c65e88a3c98206b93965bf42afc284da
disappearer <aleksa47@gmail.com>
2020-04-13 10:58:27 +0200

    Add more CI stuffs

--- config/releases.exs
+++ config/releases.exs
@@ -11,16 +11,3 @@ config :eventer_web, EventerWeb.Endpoint,
   ],
   secret_key_base: secret_key_base,
   server: true
-
-config :eventer_web, :hashid_salt, System.fetch_env!("HASH_ID_SALT")
-
-config :ueberauth, Ueberauth.Strategy.Google.OAuth,
-  client_id: System.fetch_env!("GOOGLE_CLIENT_ID"),
-  client_secret: System.fetch_env!("GOOGLE_CLIENT_SECRET")
-
-config :eventer_web, EventerWeb.Guardian,
-  issuer: "eventer_web",
-  secret_key: System.fetch_env!("GUARDIAN_SECRET_KEY")
-
-config :eventer_web, EventerWeb.Endpoint,
-  check_origin: ["//lexlabs-eventer.herokuapp.com"]
