dd6f81b2b71fe483cf0e40bbb54065e75735d3c4
disappearer <aleksa47@gmail.com>
2020-04-13 11:12:59 +0200

    Add production build time env vars

--- config/prod.exs
+++ config/prod.exs
@@ -48,3 +48,17 @@ import Config
 # Check `Plug.SSL` for all available options in `force_ssl`.
 
 # import_config "prod.secret.exs"
+
+
+config :eventer_web, :hashid_salt, System.fetch_env!("HASH_ID_SALT")
+
+config :ueberauth, Ueberauth.Strategy.Google.OAuth,
+  client_id: System.fetch_env!("GOOGLE_CLIENT_ID"),
+  client_secret: System.fetch_env!("GOOGLE_CLIENT_SECRET")
+
+config :eventer_web, EventerWeb.Guardian,
+  issuer: "eventer_web",
+  secret_key: System.fetch_env!("GUARDIAN_SECRET_KEY")
+
+config :eventer_web, EventerWeb.Endpoint,
+  check_origin: ["//lexlabs-eventer.herokuapp.com"]
